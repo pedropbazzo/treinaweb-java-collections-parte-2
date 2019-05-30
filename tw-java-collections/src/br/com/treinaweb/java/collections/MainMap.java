a7fc3e9d6998e719eb48c19ad0f2e87844a6e303
Cleber Campomori <cleber.campomori@gmail.com>
2019-05-30 11:26:41 -0300

    Aula 4.2: Explorando os mapas - LinkedHashMap

--- tw-java-collections/src/br/com/treinaweb/java/collections/MainMap.java
+++ tw-java-collections/src/br/com/treinaweb/java/collections/MainMap.java
@@ -1,6 +1,7 @@
 package br.com.treinaweb.java.collections;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import br.com.treinaweb.java.collections.models.Pessoa;
@@ -8,7 +9,7 @@ import br.com.treinaweb.java.collections.models.Pessoa;
 public class MainMap {
 
 	public static void main(String[] args) {
-		Map<String, Pessoa> mapaPessoas = new HashMap<String, Pessoa>();
+		Map<String, Pessoa> mapaPessoas = new LinkedHashMap<String, Pessoa>(16, 0.75f, true);
 		System.out.println(mapaPessoas.put("legal", new Pessoa(1, "TreinaWeb 1")));
 		System.out.println(mapaPessoas);
 		System.out.println(mapaPessoas.put("legal", new Pessoa(2, "TreinaWeb 2")));
@@ -18,6 +19,9 @@ public class MainMap {
 		mapaPessoas.put(null, new Pessoa(-2, "NULO 2"));
 		System.out.println(mapaPessoas);
 		mapaPessoas.put("teste", null);
+		System.out.println(mapaPessoas);
+		Pessoa p = mapaPessoas.get("legal");
+		System.out.println(p);
 		System.out.println(mapaPessoas);
 	}
 
